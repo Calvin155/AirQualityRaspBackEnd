9b0e3595e3a42770a31d3dd4fda4f88b76c84030
Calvin <calvinlynch181@outlook.com>
2025-01-14 22:41:33 +0000

    Update

--- Sensors/particulate_matter.py
+++ Sensors/particulate_matter.py
@@ -1,6 +1,5 @@
 import serial
 import struct
-import logging
 from Database.influxdb import InfluxDB
 
 class PM7003Sensor:
@@ -13,7 +12,6 @@ class PM7003Sensor:
         try:
             influx_db = InfluxDB()
             if self.ser.readable():
-                print("Reading Data")
                 data = self.ser.read(32)
                 
                 if data[0] == 0x42 and data[1] == 0x4D:
@@ -21,12 +19,7 @@ class PM7003Sensor:
                     pm1_0 = struct.unpack('>H', data[10:12])[0]  # PM1.0
                     pm2_5 = struct.unpack('>H', data[12:14])[0]  # PM2.5
                     pm10 = struct.unpack('>H', data[14:16])[0]    # PM10
-                    print("PM 1: " + str(pm1_0))
-                    print("PM 2.5: " + str(pm2_5))
-                    print("PM 10: " + str(pm10))
-                    print("At DB connect statememnt")
 
-                    print("Wriitng Data to DB")
                     influx_db.write_pm_data(pm1_0,pm2_5,pm10)
                     print(f"Particulate Matter 1.0: {pm1_0}, Particulate Matter 2.5: {pm2_5}, Particulate Matter 10: {pm10}")
 
