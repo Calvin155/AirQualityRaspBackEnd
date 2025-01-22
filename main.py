873e468c87aa836c4c2de01bd4c127edc363a0fb
Calvin <calvinlynch181@outlook.com>
2025-01-22 21:07:16 +0000

    test

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ while True:
         co2 = random.randint(15,20)
         temp = random.randint(9,12)
         humidity = random.randint(50,60)
+        influx_db.write_co2_temp_hum_data(co2, temp, humidity)
         time.sleep(15)
     except Exception as e:
         print("Exception: " + e)
