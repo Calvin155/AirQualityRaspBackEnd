2609c3ab52f0af8fb83adab35b8e40d68e6437c9
Calvin <calvinlynch181@outlook.com>
2025-01-26 17:21:46 +0000

    Update Logging

--- Sensors/particulate_matter.py
+++ Sensors/particulate_matter.py
@@ -21,15 +21,14 @@ class PM7003Sensor:
                     pm1_0 = struct.unpack('>H', data[10:12])[0]  # PM1.0
                     pm2_5 = struct.unpack('>H', data[12:14])[0]  # PM2.5
                     pm10 = struct.unpack('>H', data[14:16])[0]    # PM10
-                    print(f"Particulate Matter 1.0: {pm1_0}, Particulate Matter 2.5: {pm2_5}, Particulate Matter 10: {pm10} - before write to DB")
                     influx_db.write_pm_data(pm1_0,pm2_5,pm10)
-                    print(f"Particulate Matter 1.0: {pm1_0}, Particulate Matter 2.5: {pm2_5}, Particulate Matter 10: {pm10}")
+                    logging.info(f"Particulate Matter 1.0: {pm1_0}, Particulate Matter 2.5: {pm2_5}, Particulate Matter 10: {pm10}")
 
             else:
-                print("Error - No Data")
+                logging.error("Error Reading Data off PMS7003")
 
         except Exception as e:
-            print("Error - PM Sensor: " + e)
+            logging.error("Error - PM Sensor: " + e)
 
     def close_connection(self):
         if self.ser.is_open:
