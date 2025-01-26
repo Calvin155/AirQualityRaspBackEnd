57b3d1fd4a0004abe9c128122aad57aeb9aa2573
Calvin <calvinlynch181@outlook.com>
2025-01-26 17:36:28 +0000

    Main Updated

--- main.py
+++ main.py
@@ -6,7 +6,43 @@ import random
 influx_db = InfluxDB()
 pm_sensor = PM7003Sensor()
 
-# For testing pm sensor on pi & simulating CO2 sensor data
+def mock_co2_data():
+    try:
+        co2 = random.randint(15,20)
+        temp = random.randint(9,12)
+        humidity = random.randint(50,60)
+        influx_db.write_co2_temp_hum_data(co2, temp, humidity)
+    except Exception as e:
+        print(e)
+
+def full_mock():
+    # For testing without raspberry pi locally
+    try:
+        count = 0
+
+        while True:
+            random_number_one_pm = random.randint(0,30)
+            random_number_two_pm = random.randint(30,49)
+            random_number_three_pm = random.randint(50,75)
+
+            co2 = random.randint(15,20)
+            temp = random.randint(9,12)
+            humidity = random.randint(50,60)
+
+            try:
+                influx_db.write_pm_data(random_number_one_pm + count, random_number_two_pm + count, random_number_three_pm + count)
+                influx_db.write_co2_temp_hum_data(co2, temp, humidity)
+                count = count + 2
+                print("Success")
+                if count == 10:
+                    count = count - 10
+                time.sleep(10)
+            except Exception as e:
+                print("Exception: " + e)
+    except Exception as e:
+        print(e)
+
+# Main Entry Point
 while True:
     try:
         pm_sensor.read_data()
@@ -15,44 +51,4 @@ while True:
     except Exception as e:
         print("Exception: " + e)
 
-
-    def mock_co2_data():
-        try:
-            co2 = random.randint(15,20)
-            temp = random.randint(9,12)
-            humidity = random.randint(50,60)
-            influx_db.write_co2_temp_hum_data(co2, temp, humidity)
-        except Exception as e:
-            print(e)
-
-    def full_mock():
-        # For testing without raspberry pi locally
-        try:
-            count = 0
-
-            while True:
-                random_number_one_pm = random.randint(0,30)
-                random_number_two_pm = random.randint(30,49)
-                random_number_three_pm = random.randint(50,75)
-
-                co2 = random.randint(15,20)
-                temp = random.randint(9,12)
-                humidity = random.randint(50,60)
-
-                try:
-                    influx_db.write_pm_data(random_number_one_pm + count, random_number_two_pm + count, random_number_three_pm + count)
-                    influx_db.write_co2_temp_hum_data(co2, temp, humidity)
-                    count = count + 2
-                    print("Success")
-                    if count == 10:
-                        count = count - 10
-                    time.sleep(10)
-                except Exception as e:
-                    print("Exception: " + e)
-        except Exception as e:
-            print(e)
-
-
-# Will need to consider multi threading or a seperate docker container to run both sensors concurrently
-# Other sensor CO2, temp & humidity - 3 in 1 sensor - to be added later
 
