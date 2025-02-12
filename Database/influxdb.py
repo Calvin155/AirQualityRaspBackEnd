0df2ab51f6d09b9e264d10be810ad87c1b83070d
Calvin <calvinlynch181@outlook.com>
2025-02-12 20:53:07 +0000

    db Test

--- Database/influxdb.py
+++ Database/influxdb.py
@@ -34,6 +34,15 @@ class InfluxDB:
         except Exception as e:
             print("Error Connecting to Database: " + str(e))
 
+    def connected(self):
+        try:
+            if self.client.ping():
+                print("Connected & Pinging")
+            else:
+                print("No Joy")
+        except Exception as e:
+            print(e)
+
 
 
     def write_data(self, measurement, tags, fields):
@@ -50,37 +59,43 @@ class InfluxDB:
 
     def write_pm_data(self, pm1, pm2_5, pm10):
         try:
-            timestamp = datetime.utcnow().isoformat()
-            point = {
-                "measurement": "air_quality",
-                "tags": {"location": "local"},
-                "fields": {
-                    "PM1": round(pm1,2),
-                    "PM2.5": round(pm2_5,2),
-                    "PM10": round(pm10,2)
-                },
-                "time": timestamp
-            }
-            self.write_api.write(bucket=self.bucket, record=point)
-            print("Data Written Successfully to Database")
+            if self.connected():
+                timestamp = datetime.utcnow().isoformat()
+                point = {
+                    "measurement": "air_quality",
+                    "tags": {"location": "local"},
+                    "fields": {
+                        "PM1": round(pm1,2),
+                        "PM2.5": round(pm2_5,2),
+                        "PM10": round(pm10,2)
+                    },
+                    "time": timestamp
+                }
+                self.write_api.write(bucket=self.bucket, record=point)
+                print("Data Written Successfully to Database")
+            else:
+                print("Cant Connect")
         except Exception as e:
             print("Error writing data to Database" + e)
 
     def write_co2_temp_hum_data(self, co2, temp, humidity):
         try:
-            timestamp = datetime.utcnow().isoformat()
-            point = {
-                "measurement": "air_quality",
-                "tags": {"location": "local"},
-                "fields": {
-                    "CO2": round(co2,2),
-                    "Temperature": round(temp,2),
-                    "Humidity": round(humidity,2)
-                },
-                "time": timestamp
-            }
-            self.write_api.write(bucket=self.bucket, record=point)
-            print("CO2, Temp & Humidity Data Written Successfully to Database")
+            if self.connected():
+                timestamp = datetime.utcnow().isoformat()
+                point = {
+                    "measurement": "air_quality",
+                    "tags": {"location": "local"},
+                    "fields": {
+                        "CO2": round(co2,2),
+                        "Temperature": round(temp,2),
+                        "Humidity": round(humidity,2)
+                    },
+                    "time": timestamp
+                }
+                self.write_api.write(bucket=self.bucket, record=point)
+                print("CO2, Temp & Humidity Data Written Successfully to Database")
+            else:
+                print("Cant Connect")
         except Exception as e:
             print("Error writing data to Database - CO2 Sensor" + e)
 
