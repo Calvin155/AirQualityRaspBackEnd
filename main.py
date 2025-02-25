ddcad430cb42ef304334c0290bded43463636a04
Calvin <calvinlynch181@outlook.com>
2025-02-25 20:46:35 +0000

    retry sensor connection logic

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ import time
 import random
 import logging
 
+pm_sensor = PM7003Sensor()
+# Main Entry Point
 while True:
     try:
         influx_db = InfluxDB()
-        pm_sensor = PM7003Sensor()
-
-        if influx_db.connected() and pm_sensor.is_connected():
+        if pm_sensor.is_connected:
             print("Step 1")
             pm_sensor.read_data()
             print("Step Final")
             time.sleep(15)
         else:
-            print("Database or sensor not connected. Retrying in 5 seconds...")
-            time.sleep(5)
-
+            pm_sensor = PM7003Sensor()
     except Exception as e:
         print(f"Exception in main loop: {e}")
-        time.sleep(5)
-
 
 
 
