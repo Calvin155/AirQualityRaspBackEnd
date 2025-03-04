bd0978d1b6a82ea23be7031f124ee19003f50a3d
Calvin <calvinlynch181@outlook.com>
2025-03-04 09:28:05 +0000

    sensor - UART port updated & Co2 class removed

--- Sensors/particulate_matter.py
+++ Sensors/particulate_matter.py
@@ -5,7 +5,7 @@ import logging
 
 class PM7003Sensor:
     def __init__(self, baudrate=9600):
-        self.serial_port = '/dev/ttyAMA0'
+        self.serial_port = '/dev/ttyAMA2'
         self.baudrate = baudrate
         try:
             self.ser = serial.Serial(self.serial_port, self.baudrate, timeout=10)
@@ -18,16 +18,16 @@ class PM7003Sensor:
     
     def read_data(self):
         try:
-            print("Connecting To DB")
             influx_db = InfluxDB()
-            print("Writing To DB 2")
             if self.ser.readable():
                 data = self.ser.read(32)
-                if data[0] == 0x42 and data[1] == 0x4D:
+                if len(data) > 0 and data[0] == 0x42 and data[1] == 0x4D:
                     pm1_0 = struct.unpack('>H', data[10:12])[0]
                     pm2_5 = struct.unpack('>H', data[12:14])[0]
                     pm10 = struct.unpack('>H', data[14:16])[0]
-                    print("Writing To DB")
+                    print(f"Particulate Matter 1.0 (PM1.0): {pm1_0} µg/m³")
+                    print(f"Particulate Matter 2.5 (PM2.5): {pm2_5} µg/m³")
+                    print(f"Particulate Matter 10 (PM10): {pm10} µg/m³")
                     influx_db.write_pm_data(pm1_0,pm2_5,pm10)
 
             else:
