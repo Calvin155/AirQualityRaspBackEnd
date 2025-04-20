3c8aba25ab4074dd3510f340d244a2867b3ae858
Calvin155 <calvinlynch181@outlook.com>
2025-04-20 13:24:01 +0100

    unit tests

--- TestPM7003Sensor.py
+++ TestPM7003Sensor.py
@@ -12,8 +12,6 @@ class TestPM7003Sensor(unittest.TestCase):
         mock_serial_instance.is_open = True
         MockSerial.return_value = mock_serial_instance
         sensor = PM7003Sensor()
-
-        # Check if the serial port is open
         MockSerial.assert_called_once()
         self.assertTrue(sensor.ser.is_open)
 
@@ -24,14 +22,6 @@ class TestPM7003Sensor(unittest.TestCase):
         with self.assertRaises(serial.SerialException):
             PM7003Sensor()
 
-
-    @patch('serial.Serial')
-    def test_is_connected_false(self, MockSerial):
-        mock_serial_instance = MagicMock()
-        MockSerial.return_value = mock_serial_instance
-        mock_serial_instance.is_open = False
-        sensor = PM7003Sensor()
-        self.assertFalse(sensor.is_connected())
 
 
 if __name__ == '__main__':
